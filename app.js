c56867a7fae58625043902bd7f9905475ca0c295
CristianM <crism.klee@gmail.com>
2024-02-22 19:55:59 -0300

    Creacion de funciones claves en app.js

--- app.js
+++ app.js
@@ -1,3 +1,55 @@
+const matriz = [
+  ["e", "enter"],
+  ["i", "imes"],
+  ["a", "ai"],
+  ["o", "ober"],
+  ["u", "ufat"]];
+
+
 function desaparecerEncriptado() {
-    document.getElementById("area-encriptado-desencriptado").style.display = "none";
+    document.getElementById("area-encriptado-elementos").style.display = "none";
+    document.getElementById("textarea-encriptado").style.display = "block";
+    document.getElementById("boton-copiar").style.display = "block";
+
+    document.getElementById("textarea-encriptado").value = encriptar();
   }
+
+  
+
+  function encriptar(){
+    
+      let texto = document.getElementById("textareaIn").value;
+
+      for (let i=0;i<matriz.length; i++){
+          if(texto.includes(matriz[i][0])){
+              texto = texto.replaceAll(matriz[i][0], matriz[i][1]);
+          }      
+      }
+
+      return texto;
+  }
+
+  function desenciptar(){
+
+      let textoEncrip = document.getElementById("textareaIn").value;
+
+      for (let i=0; i<matriz.length; i++){
+          if (textoEncrip.includes(matriz[i][1])){
+              textoEncrip = textoEncrip.replaceAll(matriz[i][1], matriz[i][0]);
+          }
+      }
+
+      document.getElementById("textarea-encriptado").value = textoEncrip;
+
+  }
+  
+
+  function copiarTexto(){
+    let textoCopiado = document.getElementById("textarea-encriptado");
+
+    textoCopiado.select();
+    textoCopiado.setSelectionRange(0,99999); //For mobile devices
+
+    navigator.clipboard.writeText(textoCopiado.value);
+  }
+ 
